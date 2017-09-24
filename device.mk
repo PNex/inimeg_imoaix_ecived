2da475fa502e79079cb128d12611c0dc455a26a1
Marulam Heryanto Samosir <marulam.samosir@gmail.com>
2017-09-24 21:33:39 +0700

    cac

--- device.mk
+++ device.mk
@@ -37,8 +37,8 @@ TARGET_SCREEN_WIDTH := 1080
 # $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Permissions
-#    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml 
 PRODUCT_COPY_FILES += \
+	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
